49db4df3bf06d4790cd774d00bacb9957cb4a612
Dmitry Rakov <x4team@gmail.com>
2017-11-12 01:49:39 +0300

    Add finish style for index.html

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,5 +1,5 @@
-var main = function () {
-"use strict";  //Запрещает исполнение некоторых неудачных аспектов JavaScript
-window.alert("hello, world!");
-};
-$(document).ready(main);
+//var main = function () {
+//"use strict";  //Запрещает исполнение некоторых неудачных аспектов JavaScript
+//window.alert("hello, world!");
+//};
+//$(document).ready(main);
