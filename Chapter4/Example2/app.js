d58e87b832f54d4cab080d08eeb098cf91741976
Dmitry Rakov <x4team@gmail.com>
2017-11-14 23:14:21 +0300

    Add JS to comments page

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,12 +1,22 @@
 var main = function () {
 "use strict";
-$(".comment-input button").on("click", function (event) {
-var $new_comment = $("<p>"),
-comment_text = $(".comment-input input").val();
-$new_comment.text(comment_text);
+var addCommentFromInputBox = function () {
+var $new_comment;
+if ($(".comment-input input").val() !== "") {
+$new_comment = $("<p>").text($(".comment-input input").val());
+$new_comment.hide();
 $(".comments").append($new_comment);
+$new_comment.fadeIn();
+$(".comment-input input").val("");
+}
+};
+$(".comment-input button").on("click", function (event) {
+addCommentFromInputBox();
+});
+$(".comment-input input").on("keypress", function (event) {
+if (event.keyCode === 13) {
+addCommentFromInputBox();
+}
 });
 };
 $(document).ready(main);
-
-//116 page
