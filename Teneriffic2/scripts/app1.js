5dfff4580c46b3fc37fcaf9b6a0296bd937cd24c
Dmitry Rakov <x4team@gmail.com>
2017-11-20 00:13:53 +0300

    Add Teneriffic2

---
+++ Teneriffic2/scripts/app1.js
@@ -0,0 +1,14 @@
+
+
+var num;
+for (num = 1; num < 100; num++) {
+
+    if (num%3==0) {
+	console.log("Fizz");
+
+   } else if (num%5==0) {
+   	 console.log("Buzz");
+   } else { console.log("FizzBuzz"); }
+
+
+}
